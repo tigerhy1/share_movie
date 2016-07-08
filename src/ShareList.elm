51dc444f13f0d3d7751c92605ca8eceb7695bf79
tigerhy1 <tigerhy1@gmail.com>
2016-07-08 21:52:42 +0800

    use bootstrap and self-written css instead of ace-css

--- src/ShareList.elm
+++ src/ShareList.elm
@@ -8,20 +8,9 @@ import Messages exposing (..)
 showList : List ShareItem -> Html Msg
 showList items =
     div [ class "p2" ]
-        [ table []
-            [ thead []
-                [ tr []
-                    [ th [] [ text "电影" ]
-                    , th [] [ text "推荐原因" ]
-                    ]
-                ]
-            , tbody [] (List.map shareItemRow items)
-            ]
-        ]
+        (List.map shareItemRow items)
 
 shareItemRow : ShareItem -> Html Msg
 shareItemRow item =
-    tr []
-        [ td [] [ text item.movie_name] 
-        , td [] [ text item.share_comment ]
-        ]
+    div [ class "well"] 
+        [ text item.movie_name ]
--- src/View.elm
+++ src/View.elm
@@ -1,6 +1,7 @@
 module View exposing (..)
 
 import Html exposing (Html, div, text)
+import Html.Attributes exposing (class)
 import Messages exposing (Msg)
 import Models exposing (Model(..))
 import ShareList exposing (showList)
@@ -11,10 +12,17 @@ view : Model -> Html Msg
 view model =
     case model of
         ShowListModel list  -> 
-            div [] 
-                [ ShareList.showList list 
-                , showSelector  
-                ]
+            div[ class "container-fluid"]
+               [
+                div [ class "col-md-2 col-xs-1"]
+                    [],
+                div [ class "col-md-8 col-xs-10"] 
+                    [ ShareList.showList list 
+                    , showSelector  
+                    ],
+                div [ class "col-md-2 col-xs-1"]
+                    []
+               ]
         AddModel item ->
             div []
                 [ ShareAdd.addShare item
